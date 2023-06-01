5c10e0e0a461797e46b39de43e5baa6b648eacb1
jungeun-choi <jungeun5.choi@gmail.com>
2023-06-01 12:28:39 +0900

    Test - 장바구니 출력 테스트

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,8 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // 장바구니 출력 테스트
         Order order = new Order();
+        Screen screen = new Screen();
 
-        order.viewMainMenu();
+        order.addProductToCart(order.getRepo().getProduct("Burgers", 0));
+        order.addProductToCart(order.getRepo().getProduct("Burgers", 1));
+        order.addProductToCart(order.getRepo().getProduct("Burgers", 2));
+
+        screen.viewCart(order);
     }
 }
+
