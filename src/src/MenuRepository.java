dcf12b8abb25de7ce3d92d968b72d6ec85f3f90b
Jungeun Choi <jungeun5.choi@gmail.com>
2023-06-01 19:43:04 +0900

    Refactor - MenuRepository

--- src/src/MenuRepository.java
+++ src/src/MenuRepository.java
@@ -46,33 +46,18 @@ public class MenuRepository {
     /* 정보 저장 */
     private void saveInformation() {
 
-        MainMenu[] mainMenu = new MainMenu[4];
-        for(int i = 0; i < mainMenu.length; i++) { mainMenu[i] = new MainMenu(); }
-        mainMenu[0].setter("Burgers", "앵거스 비프 통살을 다져만든 버거");
-        mainMenuList.add(mainMenu[0]);
-        mainMenu[1].setter("Forzen Custard", "매장에서 신선하게 만드는 아이스크림");
-        mainMenuList.add(mainMenu[1]);
-        mainMenu[2].setter("Drinks", "매장에서 직접 만드는 음료");
-        mainMenuList.add(mainMenu[2]);
-        mainMenu[3].setter("Beer", "뉴욕 브루클린 브루어리에서 양조한 맥주");
-        mainMenuList.add(mainMenu[3]);
+        mainMenuList.add(new MainMenu("Burgers", "앵거스 비프 통살을 다져만든 버거"));
+        mainMenuList.add(new MainMenu("Forzen Custard", "매장에서 신선하게 만드는 아이스크림"));
+        mainMenuList.add(new MainMenu("Drinks", "매장에서 직접 만드는 음료"));
+        mainMenuList.add(new MainMenu("Beer", "뉴욕 브루클린 브루어리에서 양조한 맥주"));
 
-
-        ProductMenu[] productMenu = new ProductMenu[5];
-        for(int i = 0; i < productMenu.length; i++) { productMenu[i] = new ProductMenu(); }
-        productMenu[0].setter("ShackBurger", 6.9f, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        productMenuList.add(productMenu[0]);
-        productMenu[1].setter("SmokeShack", 8.9f, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        productMenuList.add(productMenu[1]);
-        productMenu[2].setter("Shroom Burger", 9.4f, "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거");
-        productMenuList.add(productMenu[2]);
-        productMenu[3].setter("Cheeseburger", 6.9f, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        productMenuList.add(productMenu[3]);
-        productMenu[4].setter("Hamburger", 5.4f, "비프패티를 기반으로 야채가 들어간 기본버거");
-        productMenuList.add(productMenu[4]);
+        productMenuList.add(new ProductMenu("ShackBurger", 6.9f, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        productMenuList.add(new ProductMenu("SmokeShack", 8.9f, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        productMenuList.add(new ProductMenu("Shroom Burger", 9.4f, "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거"));
+        productMenuList.add(new ProductMenu("Cheeseburger", 6.9f, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        productMenuList.add(new ProductMenu("Hamburger", 5.4f, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
         productMenuMap.put(mainMenuList.get(0).getName(), productMenuList);
-        productMenu = null; // 저장 후에 비우기
     }
 
 }
