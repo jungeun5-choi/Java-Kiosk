b615d269e8dedef6c2c56243082b0074379cbd0b
jungeun-choi <jungeun5.choi@gmail.com>
2023-06-01 14:42:12 +0900

    Kiosk 진입점 생성

--- src/src/Kiosk.java
+++ src/src/Kiosk.java
@@ -1,9 +1,28 @@
 import java.util.*;
 
+/// Controller
 public class Kiosk {
 
-    Order order = new Order(0);
-    Screen screen = new Screen();
+    /* 필드 */
+    private Order order;
+    private Screen screen;
 
-    // Controller
+    Kiosk(Order order, Screen screen) {
+        this.order = order;
+        this.screen = screen;
+    }
+
+
+    /* 메서드 */
+
+    // 키오스크 진입점, 외부에서 호출할 때 사용
+    public void startKiosk() {
+        processKiosk();
+    }
+
+    // 실제로 돌아가는 키오스크
+    private void processKiosk() {
+
+    }
+
 }
